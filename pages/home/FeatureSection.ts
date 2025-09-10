05f801032156cc0576585f91c0e0c65dbd4adf1b
NghiChris <cnghi8995@gmail.com>
2025-09-10 21:31:23 +0700

    first commit

--- pages/home/FeatureSection.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class FeatureSection {
-  constructor(private page: Page) {}
-
-  async getFeatures() {
-    return this.page.locator('.feature-item').allTextContents();
-  }
-}
