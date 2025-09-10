b4529e929f2924a71e7c757c12ab0e18fc80b826
NghiChris <cnghi8995@gmail.com>
2025-09-10 21:31:23 +0700

    first commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# Test05-Group05-PlayWright
--- pages/home/HeroSection.ts
+++
@@ -1,9 +0,0 @@
-import { Page } from '@playwright/test';
-
-export class HeroSection {
-  constructor(private page: Page) {}
-
-  async getTitle() {
-    return this.page.textContent('.hero h1');
-  }
-}
