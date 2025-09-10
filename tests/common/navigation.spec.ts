8f0dc4d02a9f85d1f36d220e18d4f26ad5394ebf
Huỳnh Thùy <bichthuy060590@gmail.com>
2025-09-10 08:25:31 +0700

    Init

--- tests/common/navigation.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { HomePage } from '../../pages/home/HomePage';
-
-test('navigation menu works', async ({ page }) => {
-  const home = new HomePage(page);
-  await home.goto();
-  await home.header.navigateTo('Blog');
-  await expect(page).toHaveURL(/.*blog/);
-});
